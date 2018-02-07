fd7f4eef4591286f5bb675288c8b7f6d6cb45c4d
DavidYunga <33554917+DavidYunga@users.noreply.github.com>
2018-02-07 11:09:36 -0500

    Add files via upload

--- src/figurasgraficas/Graficador.java
+++ src/figurasgraficas/Graficador.java
@@ -1,75 +1,101 @@
-/*  
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package figurasgraficas;
+package repasoexamen1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import javafx.scene.chart.Chart;
 
 /**
  *
  * @author miltonlab
  */
 public class Graficador {
-   
-    ArrayList<FiguraGeometrica> figuras = new ArrayList<FiguraGeometrica>();
-    
-    private String archivo;
 
-    public String getArchivo() {
-        return archivo;
+    String datos = "";
+    static ArrayList<FiguraGeometrica> figuras = new ArrayList<FiguraGeometrica>();
+
+    public static void agregar(FiguraGeometrica f) {
+        figuras.add(f);
     }
 
-    public void setArchivo(String archivo) {
-        this.archivo = archivo;
-    }
-  
-    public void agregarFigura(FiguraGeometrica f){
-        this.figuras.add(f);
-        this.grabar();
-       
-    }
-    public void mover(double x, double y){
+    public static void moverFiguras(int dx, int dy) {
         for (FiguraGeometrica figura : figuras) {
-            figura.mover(x, y);
+            figura.mover(dx, dy);
         }
-        this.grabar();
     }
-    
-    public void grabar() {
+
+    public static void grabar() {
         PrintWriter pw = null;
         try {
-            FileWriter fw = new FileWriter(archivo, true);
-            pw = new PrintWriter(fw);    
+            FileWriter fw = new FileWriter("figuras.csv", true);
+            pw = new PrintWriter(fw);
         } catch (Exception e) {
-            System.out.println("Error al leer el archivo: " + e);
+            System.out.println("Error en la lectura del archivo");
         }
         for (FiguraGeometrica figura : figuras) {
-            String linea = "";
-            if (figura instanceof Cuadrado){
-                Cuadrado c = ((Cuadrado)figura);
-                linea = "Cuadrado;" + figura.nombre + ";" + 
-                        c.vertice1 + c.vertice2 + c.vertice3 + c.vertice4;
-            }
             if (figura instanceof Circulo) {
-                Circulo c = ((Circulo)figura);
-                linea = "Circulo;" + figura.nombre + ";" + c.centro;
+                Circulo c = (Circulo) figura;
+                String line = "Circulo" + ";" + c.getNombre() + ";" + c.getCentro() + ";" + c.getRadio();
+                pw.println(line);
             }
-            pw.println(linea);
+            if (figura instanceof Rectangulo) {
+                Rectangulo rec = (Rectangulo) figura;
+                String linea = "Rectangulo" + ";" + rec.getNombre() + ";" + rec.getV1() + ";" + rec.getV2() + ";" + rec.getV3() + ";" + rec.getV4();
+                pw.println(linea);
+            }
         }
         pw.close();
     }
-    
-    public void recargar (){
-        /*
-         * Completar este método: 
-         * El objetivo es leer todos los datos del archivo de texto 
-         * y crear los objetos 'FiguraGeometrica' para agregarlos a
-         * la colección 'figuras'
-        */
+
+    public static String recargar() {
+        BufferedReader leer = null;
+        String lector = "";
+        try {
+
+            FileReader f = new FileReader("D://Informacion//documentos//NetBeansProjects//RepasoExamen1//figuras.csv");
+            leer = new BufferedReader(f);
+            String linea;
+            while ((linea = leer.readLine()) != null) {
+                String[] registro = linea.split(";");
+                if (registro[0].equals("Circulo")) {
+                    String puntosCadena = registro[2];
+                    String[] puntos = puntosCadena.split(",");
+                    int xc = Integer.parseInt(puntos[0]);
+                    int yc = Integer.parseInt(puntos[1]);
+                    double radio = Double.parseDouble(registro[3]);
+                    Circulo circulo = new Circulo();
+                    circulo.radio = radio;
+                    circulo.centro = new Punto(xc, yc);
+                    circulo.nombre = registro[1];
+                    Graficador.agregar(circulo);
+                    lector += circulo.toString() + "\n";
+                } else {
+                    String puntosV1 = registro[2];
+                    String[] punto1 = puntosV1.split(",");
+                    int xv1 = Integer.parseInt(punto1[0]);
+                    int yv1 = Integer.parseInt(punto1[1]);
+                    String puntosV3 = registro[4];
+                    String[] punto3 = puntosV3.split(",");
+                    int xv3 = Integer.parseInt(punto3[0]);
+                    int yv3 = Integer.parseInt(punto3[1]);
+                    Rectangulo rectangulo = new Rectangulo(new Punto(xv1, yv1), new Punto(xv3, yv3));
+                    rectangulo.nombre=registro[1];
+                    Graficador.agregar(rectangulo);
+                    lector += rectangulo.toString() + "\n";
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error en la lectura del archivo");
+        }
+        return lector;
     }
+
 }
